688e649b9e500457ada2d4086325561a271d16a9
Rusiru Rathmina <rusirurathminarr@gmail.com>
2023-06-27 09:33:36 +0530

    Done

--- client/src/components/RestaurantList.jsx
+++ client/src/components/RestaurantList.jsx
@@ -2,6 +2,7 @@ import React, {useContext, useEffect} from 'react'
 import { useNavigate } from "react-router-dom";
 import RestaurantFinder from '../apis/RestaurantFinder'
 import { RestaurantsContext } from '../context/RestaurantsContext';
+import StarRating from './StarRating';
 
 const RestaurantList = (props) => {
 
@@ -43,6 +44,19 @@ const RestaurantList = (props) => {
         navigate(`/restaurants/${id}`)
     }
 
+    const renderRating = (restaurant) => {
+
+        if(!restaurant.count){
+            return <span className='text-warning ml-1'>0 reviews</span>;
+        }
+        return (
+            <>
+                <StarRating rating={restaurant.id} />
+                <span className='text-warning ml-1'>({restaurant.count})</span>
+            </>
+        );
+    };
+
   return (
     <div>
         <div className="list-group">
@@ -64,7 +78,7 @@ const RestaurantList = (props) => {
                                 <td>{restaurant.name}</td>
                                 <td>{restaurant.location}</td>
                                 <td>{"$".repeat(restaurant.price_range)}</td>
-                                <td>reviews</td>
+                                <td>{renderRating(restaurant)}</td>
                                 <td><button onClick={(e) => handleUpdate(e, restaurant.id)} className="btn btn-warning">Update</button></td>
                                 <td><button onClick={(e) => handleDelete(e, restaurant.id)} className="btn btn-danger">Delete</button></td>
                             </tr>
--- server/server.js
+++ server/server.js
@@ -14,12 +14,14 @@ app.use(express.json());
 // Get all Restaurants
 app.get("/api/v1/restaurants", async (req, res) => {
     try{
-        const results = await db.query("SELECT * FROM restaurant");
+        // const results = await db.query("SELECT * FROM restaurant");
+        const restaurantRatingsData = await db.query("SELECT * FROM restaurant left join (select restaurant_id, COUNT(*), TRUNC(AVG(rating),1) AS average_rating FROM reviews GROUP BY restaurant_id) reviews on restaurant.id =reviews.restaurant_id;");
+        
         res.status(200).json({ 
             status: "Success",
-            results: results.rows.length,
+            results: restaurantRatingsData.rows.length,
             data: {
-                 restaurants: results.rows,
+                 restaurants: restaurantRatingsData.rows,
             },
         });
     } catch (err) {
@@ -31,9 +33,12 @@ app.get("/api/v1/restaurants", async (req, res) => {
 app.get("/api/v1/restaurants/:id", async (req, res) => {
 
     try {
-        const restaurant = await db.query(`SELECT * FROM restaurant WHERE id = $1`, [req.params.id]);
+        const restaurant = await db.query(`SELECT * FROM restaurant left join (select restaurant_id, COUNT(*), TRUNC(AVG(rating),1) AS average_rating FROM reviews GROUP BY restaurant_id) reviews on restaurant.id =reviews.restaurant_id WHERE id = $1`, [req.params.id]);
 
         const reviews = await db.query(`SELECT * FROM reviews WHERE restaurant_id = $1`, [req.params.id]);
+
+
+
         res.status(200).json({
             status: "succes",
             data: {
